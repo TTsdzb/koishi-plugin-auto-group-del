d586f41f38edc81e5d0323247481414204aecb90
TTsdzb <ttsdzb@outlook.com>
2024-07-10 11:27:35 +0800

    feat: initial commit

--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-auto-group-del?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-auto-group-del)
 
-适用于Koishi的自动退群插件
+适用于 Koishi 的自动退群插件。
+
+Bot 被禁言时自动退群。
+
+由于 Koishi 通用 API 并没有禁言事件或退群接口（其他平台也不一定支持退群），该插件是针对 Onebot 适配器实现的，在其他平台不工作。
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema, Session } from "koishi";
+import {} from "koishi-plugin-adapter-onebot";
 
-export const name = 'auto-group-del'
+export const name = "auto-group-del";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const logger = new Logger("auto-group-del");
+
+  // This event is not defined
+  // Should be investigated
+  // @ts-ignore
+  ctx.on("guild-member", async (session: Session) => {
+    if (session.platform !== "onebot" || session.subtype !== "ban") return;
+
+    logger.debug("SelfId:", session.selfId);
+    logger.debug("UserId:", session.userId);
+    logger.debug("GuildId:", session.guildId);
+
+    if (session.selfId !== session.userId) return;
+    logger.debug("Self mute detected in group:", session.guildId);
+
+    // Lagrange.Onebot uses strict type when parsing requests
+    await session.onebot.setGroupLeave(parseInt(session.guildId));
+  });
 }
